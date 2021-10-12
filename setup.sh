4c5f070fdc3be32dfa627ba2cc86fd3bb3a91f04
Zachary Sy <zacharybsy@icloud.com>
2021-10-12 11:07:01 -0400

    modify readme and add textworld dependency

--- setup.sh
+++ setup.sh
@@ -10,4 +10,5 @@ source $VENVNAME/bin/activate
 # Install spaCy: https://spacy.io/usage
 pip install -U pip setuptools wheel
 pip install -U spacy[cuda112]         # modify for a given CUDA version
+pip install -U textworld
 python -m spacy download en_core_web_trf
