3fc8e8d6f537a928f87f06f5ffee7cc672376e3c
Patrick Faley <patrick.faley@gmail.com>
2021-10-11 22:50:16 -0500

    Added .gitignore and setup script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Constants
+VENVNAME="nlp_venv"
+
+# Create virtual environment
+python -m venv $VENVNAME
+source $VENVNAME/bin/activate
+
+# Install spaCy: https://spacy.io/usage
+pip install -U pip setuptools wheel
+pip install -U spacy[cuda112]         # modify for a given CUDA version
+python -m spacy download en_core_web_trf
