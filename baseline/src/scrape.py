721d20cac85be83743073bef1f82f3684e3eed72
Patrick <psoga117@gmail.com>
2021-10-13 08:13:54 -0700

    docstrings for scrape

--- baseline/src/scrape.py
+++ baseline/src/scrape.py
@@ -22,6 +22,9 @@ headers = {
 
 
 def get_soup_content(url: str):
+    """
+    Util for getting soup content from url.
+    """
     request = requests.get(url, headers)
     content = request.content.decode('utf-8')
     soup = bs4.BeautifulSoup(content, features="html.parser")
@@ -29,10 +32,16 @@ def get_soup_content(url: str):
 
 
 def scrape_text(read_online_url: str):
+    """
+    Scrape the book using the "Read Online" option
+    and write all of the text blocks that make up the novel
+    to a file line-by-line.
+    """
     soup = get_soup_content(read_online_url)
     blocks = map(lambda fragment: fragment.get_text(
         strip=True).lower(), soup.find_all('p', class_=''))
     for block in blocks:
+        # Split lines by ., ?, !, and any of the previous followed by a quote.
         lines = re.split(r'(?:(?<=[.!?])|(?<=[.!?]["”]))\s+', block)
         for line in lines:
             with open('novels.txt', 'a+') as file:
@@ -40,6 +49,10 @@ def scrape_text(read_online_url: str):
 
 
 def scrape_book_link(book_url: str):
+    """
+    Check validity of the given book page.
+    Reject if the novel isn't in English or it's not readable online.
+    """
     soup = get_soup_content(book_url)
     bibrec = soup.find('table', class_='bibrec')
     language = bibrec.find(
@@ -58,13 +71,17 @@ def scrape_book_link(book_url: str):
 
 
 def scrape_page(idx: int, book_titles: set):
+    """
+    Scrape whole page of books. Each page has 25 books.
+    """
     idx_url = base_url + f"/ebooks/subject/138?start_index={idx}"
     soup = get_soup_content(idx_url)
 
     book_links = soup.find_all('li', class_='booklink')
 
     for book_link in tqdm.tqdm(book_links):
-        title = book_link.find('span', class_='title')
+        title = book_link.find('span', class_='title').text
+        # Avoid scraping copies of the same book.
         if title in book_titles:
             continue
         book_titles.add(title)
@@ -72,8 +89,11 @@ def scrape_page(idx: int, book_titles: set):
         scrape_book_link(base_url + book_url_ending)
 
 
-def driver_scrape(max_pages, n_threads):
-    page_indices = [i for i in range(1, (max_pages*25)+1, 25)]
+def driver_scrape(pages, n_threads):
+    """
+    Main driver function for scraping pages in parallel.
+    """
+    page_indices = [i for i in range(1, (pages*25)+1, 25)]
     book_titles = set()
     partial_scrape_page = partial(scrape_page, book_titles=book_titles)
 
