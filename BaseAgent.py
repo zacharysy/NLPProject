86e21aab3ebf38b03434bc7da7f7b059c6558fab
Zachary Sy <zacharybsy@icloud.com>
2021-11-04 19:07:58 -0400

    Add zork benchmark

--- BaseAgent.py
+++ BaseAgent.py
@@ -1,18 +1,13 @@
+import sys
 import textworld
-import textworld.challenges
-from textworld.generator.game import GameOptions
-import textworld.generator
-
 sys.path.append("./")
-from baseline.nounverb import NounVerb
-from baseline.utils import extract_nouns
 
 class BaseAgent(textworld.Agent):
     def act(self, game_state, reward, done):
         return self.callModel(game_state.feedback)
 
     def callModel(self, text):
-        pass
+        return "open mailbox"
 
 if __name__ == "__main__":
     pass
