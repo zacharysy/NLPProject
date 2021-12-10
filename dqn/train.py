18a75c99b132fadf9dab8044afe3ab356a233832
patricksoga <psoga@nd.edu>
2021-12-09 22:06:30 -0800

    training code

--- dqn/train.py
+++ dqn/train.py
@@ -1,11 +1,15 @@
+import collections
 import textworld
 import sys
 import torch
 import re
 
+from itertools import groupby
+from random import choice
+from typing import List
+from dataclasses import dataclass
 from BaseAgent import DQAgent
 from random import randint, random
-from dqn import DQN
 from util import Vocab
 from argparse import ArgumentParser
 
@@ -18,7 +22,7 @@ def contains_digit(in_str: str) -> bool:
     return sum([i.isdigit() for i in in_str]) > 0
 
 
-def preprocess_line(in_str: str, start_symbol: str = None) -> [str]:
+def preprocess_line(in_str: str, start_symbol: str = None) -> List[str]:
     """
     This function is used to preprocess a single line of the input.
     """
@@ -55,28 +59,84 @@ def read_data():
     return words, actions
 
 
+@dataclass
+class ReplayMemory:
+    s_t: str                # state
+    a_t: int                # action
+    r_t: int                # reward
+    s_next: str             # next state
+    p_t: int                # priority
+
+
+def random_sample(iterable, n):
+    len_iter = len(iterable)
+    return [choice(iterable) for _ in range(n if n <= len_iter else len_iter)]
+
+
+def mini_sample(rho, replay_memories: List[ReplayMemory]) -> List[ReplayMemory]:
+    num_replay_mems = len(replay_memories)
+    high_rhos, low_rhos = groupby(replay_memories, lambda x: x.p_t == 1)
+    high_rho_ratio = len(list(high_rhos))/num_replay_mems
+
+    if high_rho_ratio <= rho:
+        return random_sample(replay_memories, 1000)
+
+    target = int(len(num_replay_mems) * high_rho_ratio)
+    train_memories = random_sample(
+        high_rhos, target) + random_sample(low_rhos, 1000 - target)
+    return train_memories
+
+
 def main(args):
     word_vocab, action_vocab = read_data()
     dims = 200
     epsilon = 0.1
-    dqn = DQN(word_vocab, action_vocab, dims)
-    agent = DQAgent()
+    rho = 0.25
+    gamma = 0.5
+    agent = DQAgent(word_vocab, action_vocab, dims)
+    replay_memories: List[ReplayMemory] = []
+
+    optim = torch.optim.Adam(agent.dqn.parameters(), lr=0.01)
+    loss_fn = torch.nn.MSELoss()
 
     for _ in range(args.episodes):
+        total_loss = 0
         env = textworld.start(zorkPath)
         game_state = env.reset()
         reward, done, moves = 0, False, 0
         desc = ['<CLS>', *preprocess_line(game_state['raw'])]
         for t in range(maxMoves):
-            encoding = dqn.encode(desc)
+            encoding = agent.dqn.encode(desc)
             if random() < epsilon:
                 action_num = randint(0, len(action_vocab))
             else:
-                dist = dqn(encoding)
-                action_num = torch.argmax(dist)
+                dist = agent.dqn(encoding)
+                action_num = dist.argmax()
+
             command = agent.callModel(action_num)
             game_state, reward, done = env.step(command)
-            # if reward > 0:
+            priority = 1 if reward > 0 else 0
+            replay_memories.append(ReplayMemory(
+                desc, action_num, reward, game_state if not done else 'done', priority))
+
+            train_mems = mini_sample(rho, replay_memories)
+            for t in train_mems:
+                r = torch.tensor(t.r_t)
+                if t.s_next == 'done':
+                    y = r
+                else:
+                    next_enc = agent.dqn.encode(t.s_next)
+                    curr_enc = agent.dqn.encode(t.s_t)
+
+                    max_r = torch.max(agent.dqn(next_enc))
+                    y = r + (gamma * max_r)
+                loss = loss_fn(y, agent.dqn(curr_enc)[t.a_t])
+
+                optim.zero_grad()
+                loss.backward()
+                optim.step()
+
+                total_loss += loss.detach().item()
 
 
 if __name__ == "__main__":
