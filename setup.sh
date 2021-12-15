1df807029507bbeea52a2523452be6bd921b673d
Patrick Faley <patrick.faley@gmail.com>
2021-12-15 15:50:09 -0600

    Streamlined setup.sh

--- setup.sh
+++ setup.sh
@@ -9,18 +9,21 @@ source $VENVNAME/bin/activate
 
 # Install spaCy: https://spacy.io/usage
 pip install -U pip setuptools wheel
-pip install -U spacy[cuda112]         # modify for a given CUDA version
-pip install -U textworld
-pip install -U tqdm
-pip install -U bs4
-pip install -U diaparser # Dependency Parser
-pip install -U pymagnitude # Word Embeddings helper
-pip install -U nltk
-pip install -U scikit-learn
-pip install -U stanford-openie
+pip install -r requirements.txt
+# pip install -U spacy[cuda112]         # modify for a given CUDA version
+# pip install -U textworld
+# pip install -U tqdm
+# pip install -U bs4
+# pip install -U diaparser # Dependency Parser
+# pip install -U pymagnitude # Word Embeddings helper
+# pip install -U nltk
+# pip install -U scikit-learn
+# pip install -U stanford-openie
+
+# Install extra stuff
 python -m nltk.downloader all
-python -m spacy download en_core_web_trf
-python -m spacy download en
+# python -m spacy download en_core_web_trf
+# python -m spacy download en
 
 # note - pymagnitude seems to have issues with spacy, may need to uninstall
 
